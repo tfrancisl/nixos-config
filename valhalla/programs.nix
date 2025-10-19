f11d05c3271a426df24b94411ff4d8a6e5a2fd3b
tfrancisl <tfrancislester@gmail.com>
2025-10-19 12:20:11 -0400

    Prepare for declarative hyprland conf.

--- valhalla/programs.nix
+++ valhalla/programs.nix
@@ -1,12 +1,14 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
+  imports = [
+    inputs.hyprland.nixosModules.default
+  ];
   # Track non-free software explicitly
   nixpkgs.config.allowUnfreePredicate =
     pkg:
     builtins.elem (pkgs.lib.getName pkg) [
       "nvidia-x11"
       "nvidia-settings"
-      "nvidia-persistenced"
       "discord"
       "steam"
       "steam-unwrapped"
@@ -15,51 +17,6 @@
       "firefox-bin-unwrapped"
       "geekbench"
     ];
-
-  programs = {
-    uwsm = {
-      enable = true;
-    };
-    hyprland = {
-      enable = true;
-      withUWSM = true;
-    };
-    fish.enable = true;
-    firefox = {
-      enable = true;
-      package = pkgs."firefox-bin"; # official firefox dist
-    };
-    steam = {
-      enable = true;
-    };
-    gamemode = {
-      enable = true;
-      settings = {
-        general = {
-          softrealtime = "auto";
-          renice = 15;
-        };
-      };
-    };
-  };
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   extraConfig.pipewire."92-low-latency" = {
-  #     "context.properties" = {
-  #       "default.clock.rate" = 48000;
-  #       "default.clock.quantum" = 512;
-  #       "default.clock.min-quantum" = 128;
-  #       "default.clock.max-quantum" = 2048;
-  #     };
-  #   };
-  # };
-  #
 
   environment.systemPackages = with pkgs; [
 
@@ -80,4 +37,35 @@
     geekbench
 
   ];
+
+  programs = {
+    uwsm.enable = true;
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+    fish.enable = true;
+    firefox = {
+      enable = true;
+      package = pkgs."firefox-bin"; # official firefox dist
+    };
+    steam.enable = true;
+    gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          softrealtime = "auto";
+          renice = 15;
+        };
+      };
+    };
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire.enable = true;
+
 }
