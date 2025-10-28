f758f3af630763e4c7ca0eeba58f0437c1e400a9
tfrancisl <tfrancislester@gmail.com>
2025-10-28 09:05:27 -0400

    Try to actually use cachix for hyprland

--- valhalla/settings.nix
+++ valhalla/settings.nix
@@ -20,10 +20,6 @@
       "nix-command"
       "flakes"
     ];
-    # get hyprland from cachix
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     auto-optimise-store = true;
   };
   nix.optimise.automatic = true;
