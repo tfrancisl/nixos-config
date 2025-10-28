10614f709b725f201d7cb54b1b34de2bf1da261b
tfrancisl <tfrancislester@gmail.com>
2025-10-28 09:05:27 -0400

    Try to actually use cachix for hyprland

--- flake.nix
+++ flake.nix
@@ -1,16 +1,20 @@
 {
   description = "NixOS flake";
 
+  nixConfig = {
+    # get hyprland from cachix
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    hyprland = {
-      url = "github:hyprwm/hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    hyprland.url = "github:hyprwm/hyprland";
   };
 
   outputs = inputs @ {self, ...}: let
@@ -22,7 +26,6 @@
       imports = [
         {
           flake.nixosConfigurations.valhalla = inputs.nixpkgs.lib.nixosSystem {
-            inherit specialArgs;
             modules = [
               ./valhalla/settings.nix
               ./valhalla/hardware
@@ -30,6 +33,7 @@
               ./valhalla/hyprland
               ./valhalla/system
             ];
+            inherit specialArgs;
           };
         }
       ];
