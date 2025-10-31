e12b78013b31d17be9f868bb9e6cab097dc67c7f
tfrancisl <tfrancislester@gmail.com>
2025-10-31 09:51:51 -0400

    Actually use caches for real this time.

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   description = "NixOS flake";
 
   nixConfig = {
-    # get hyprland from cachix
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extra-substituters = ["https://hyprland.cachix.org"];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
   };
 
   inputs = {
