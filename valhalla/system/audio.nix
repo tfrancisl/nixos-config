ac877b86c2cce3e502ec5af770b242b1531fdc8b
tfrancisl <tfrancislester@gmail.com>
2025-10-31 17:18:17 -0400

    headset devices

--- valhalla/system/audio.nix
+++ valhalla/system/audio.nix
@@ -1,9 +1,51 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    qpwgraph
-    pavucontrol
-  ];
-  services.pulseaudio.enable = false;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  # environment.systemPackages = with pkgs; [
+  #   qpwgraph
+  #   pavucontrol
+  # ];
+  services.pulseaudio.enable = lib.mkForce false;
   security.rtkit.enable = true;
-  services.pipewire.enable = true;
+  services.pipewire = {
+    enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "microphone" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "alsa_input.pci-0000_0b_00.3.analog-stereo";
+                }
+              ];
+              actions = {
+                update-props = {
+                };
+              };
+            }
+          ];
+        };
+        "headphone" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "alsa_card.pci-0000_0b_00.3";
+                }
+              ];
+
+              actions = {
+                update-props = {
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
 }
