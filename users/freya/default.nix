179e9535ec48ac203bd46d99e328ff51676d0ae7
tfrancisl <tfrancislester@gmail.com>
2025-11-09 16:56:06 -0500

    Refactor user config to support NixOS and nix-darwin

--- users/freya/default.nix
+++ users/freya/default.nix
@@ -1,5 +1,15 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
+    (import ../common {
+      inherit pkgs;
+      username = "freya";
+      zedSettings = ../common/zed/settings.json;
+    })
     ./packages.nix
   ];
+
+  # programs.git.config.user = {
+  #   name = "Your Name";
+  #   email = "your@email.com";
+  # };
 }
