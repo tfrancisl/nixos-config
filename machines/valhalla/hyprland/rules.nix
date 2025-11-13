4ae44799b27e2ca86ca39ee06c6bd166e3588f58
tfrancisl <tfrancislester@gmail.com>
2025-11-13 16:21:32 -0500

    hyprland: update to windowrulesv3
    and update flake

--- machines/valhalla/hyprland/rules.nix
+++ machines/valhalla/hyprland/rules.nix
@@ -1,26 +1,20 @@
 {
   programs.hyprland.settings = {
-    windowrulev2 = [
-      # floating have special active/inactive border
-      "bordercolor rgba(111212ff) rgba(111212ff), floating:1"
-      "bordercolor rgba(4122d5ff) rgba(15f33dff), focus:1, floating:1"
-      # alacritty always floats
-      "float, class:^(Alacritty)$"
-      "size 50% 50%, class:^(Alacritty)$"
-      "move 12.5% 12.5%, class:^(Alacritty)$"
-      "keepaspectratio, class:^(Alacritty)$"
-      "opacity 1.0 0.34, class:^(Alacritty)$"
-      # no maxim pls
-      "suppressevent maximize, class:.*"
-    ];
     workspace = [
       # turn off gaps when tiled window count = 1
       "w[tv1], gapsout:0, gapsin:0"
     ];
     windowrule = [
       # turn off borders on solo windows
-      "bordersize 0, floating:0, onworkspace:w[tv1]"
-      "rounding 0, floating:0, onworkspace:w[tv1]"
+      "match:workspace w[tv1], match:float false, border_size 0, rounding 0"
+
+      # floating have special active/inactive border
+      "match:float true, match:focus false, border_color rgba(111212ff) rgba(111212ff)"
+      "match:float true, match:focus true, border_color rgba(4122d5ff) rgba(15f33dff)"
+
+      "match:class ^(Alacritty)$, float on, move 12.5% 12.5%, move 12.5% 12.5%, keep_aspect_ratio on, opacity 1.0 0.34"
+
+      "match:class .*, suppress_event maximize"
     ];
   };
 }
