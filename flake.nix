ccf16c14eb7470e723e58362a2211b5963e4c5d4
tfrancisl <tfrancislester@gmail.com>
2025-11-26 18:16:05 -0500

    hyprcursor comes with hyprland flake

--- flake.nix
+++ flake.nix
@@ -14,10 +14,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+
     hyprland.url = "github:hyprwm/hyprland";
   };
 
