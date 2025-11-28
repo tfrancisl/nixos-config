8b1173fcb797d0bc52bff7a3a912ddc693b18270
tfrancisl <tfrancislester@gmail.com>
2025-11-28 09:43:11 -0500

    Move audio cfg into valhalla/programs

--- machines/valhalla/hyprland/default.nix
+++ machines/valhalla/hyprland/default.nix
@@ -4,6 +4,9 @@
     withUWSM = true;
   };
 
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
   imports = [
     inputs.hyprland.nixosModules.default # Hyprland maintained nixos mod
     ./settings.nix
