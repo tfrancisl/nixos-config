43eaa0b9cc8899667072b783219b8a7b74be3f80
tfrancisl <tfrancislester@gmail.com>
2025-11-28 10:36:28 -0500

    Add uv + python3.14 setup

--- users/freya/default.nix
+++ users/freya/default.nix
@@ -5,4 +5,13 @@
       username = "freya";
     })
   ];
+  # uv + python setup
+  # https://nixos.org/manual/nixpkgs/unstable/#sec-uv
+  users.users.freya.packages = with pkgs; [
+    uv
+    python314
+  ];
+
+  environment.sessionVariables.UV_PYTHON = "${pkgs.python314}/bin/python";
+  environment.sessionVariables.UV_PYTHON_DOWNLOADS = "never";
 }
