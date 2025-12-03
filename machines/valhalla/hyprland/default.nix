77c578883bb2452f77cb84ea633c07347eac6af9
tfrancisl <tfrancislester@gmail.com>
2025-12-03 16:16:58 -0500

    Add quickshell

--- machines/valhalla/hyprland/default.nix
+++ machines/valhalla/hyprland/default.nix
@@ -1,17 +1,28 @@
-{inputs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    quickshell
+  ];
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
   imports = [
-    inputs.hyprland.nixosModules.default # Hyprland maintained nixos mod
     ./settings.nix
     ./theme.nix
     ./binds.nix
     ./rules.nix
+    # Hyprland maintained nixos mod
+    # https://github.com/hyprwm/Hyprland/blob/main/flake.nix
+    # https://github.com/hyprwm/Hyprland/blob/main/nix/module.nix
+    inputs.hyprland.nixosModules.default
   ];
 }
