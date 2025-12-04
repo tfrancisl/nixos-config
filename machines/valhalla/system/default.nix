8454b2cfd5f14d5ecb66f9836604d98ab6922945
tfrancisl <tfrancislester@gmail.com>
2025-12-04 10:13:07 -0500

    stop using flake-parts

--- machines/valhalla/system/default.nix
+++ machines/valhalla/system/default.nix
@@ -1,8 +1,24 @@
-{...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
+  users.users.${username} = {
+    isNormalUser = true;
+    description = "${username}'s user account";
+    shell = pkgs.fish;
+    extraGroups = [
+      "networkmanager"
+      "input"
+      "wheel"
+      "video"
+      "audio"
+      "tss"
+    ];
+  };
+
   imports = [
     ./greeting.nix
-    ./programs.nix
     ./system.nix
-    ./users.nix
   ];
 }
