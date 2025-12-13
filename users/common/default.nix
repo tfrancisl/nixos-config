b242bf5a6a02d9c11ec8ebec6e836302021255d0
tfrancisl <tfrancislester@gmail.com>
2025-12-12 19:14:17 -0500

    Add nh, disable nix store optimizer

--- users/common/default.nix
+++ users/common/default.nix
@@ -78,6 +78,13 @@ in {
       fish = {
         enable = true;
       };
+
+      nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 10d --keep 4 --optimise";
+        flake = "/home/${username}/nixos-config";
+      };
     };
 
     environment.shellAliases = {
