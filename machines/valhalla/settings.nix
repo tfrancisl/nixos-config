9e8e93662de1083d3abf8c4efae7fdb8112f2e81
tfrancisl <tfrancislester@gmail.com>
2025-12-14 13:22:32 -0500

    Restructure

--- machines/valhalla/settings.nix
+++ machines/valhalla/settings.nix
@@ -1,40 +1,10 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{config, ...}: let
+  username = config.system_user.username;
+in {
   time.timeZone = "America/New_York"; # EST/EDT
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Track non-free software explicitly
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (pkgs.lib.getName pkg) [
-      "nvidia-x11"
-      "nvidia-settings"
-      "discord"
-      "steam"
-      "steam-unwrapped"
-      "spotify"
-      "firefox-bin"
-      "firefox-bin-unwrapped"
-      "lunar-client"
-      "lunarclient"
-      "prismlauncher"
-    ];
-
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    useOSProber = true;
-  };
-
-  networking = {
-    hostName = "valhalla";
-    networkmanager.enable = true;
-    useDHCP = lib.mkDefault true;
-  };
-
-  system.stateVersion = "25.05"; # Do not change this!
+  nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
     experimental-features = [
@@ -44,19 +14,7 @@
     fallback = true;
     connect-timeout = 10;
 
-    # get hyprland from cachix
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-
-    trusted-substituters = ["https://hyprland.cachix.org"];
-
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-
     # Can add more settings to nixConfig in flake
-    trusted-users = ["freya"];
+    trusted-users = [username];
   };
 }
--- machines/valhalla/system.nix
+++ machines/valhalla/system.nix
@@ -16,6 +16,12 @@
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/59dc666f-1a13-4028-a4a0-90dcf6333084";
     fsType = "ext4";
@@ -25,4 +31,12 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  networking = {
+    hostName = "valhalla";
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+  };
+
+  system.stateVersion = "25.05"; # Do not change this!
 }
