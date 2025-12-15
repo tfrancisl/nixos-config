cec962f68cfac8429405d184572da37cc17417ba
tfrancisl <tfrancislester@gmail.com>
2025-12-15 12:27:52 -0500

    Fix Alacritty to 16:9 regardless of monitor aspect

--- machines/valhalla/hyprland/rules.nix
+++ machines/valhalla/hyprland/rules.nix
@@ -8,7 +8,8 @@
       # turn down borders on solo windows
       "match:workspace w[tv1], match:float false, border_size 4"
 
-      "match:class ^(Alacritty)$, float on, move 0.125*monitor_w 0.125*monitor_h, size 0.5*monitor_w 0.5*monitor_h, keep_aspect_ratio on, opacity 1.0 0.34"
+      # Alacritty starts floating and at 16:9, regardless of monitor size.
+      "match:class ^(Alacritty)$, float on, move 0.2*monitor_w 0.2*monitor_h, size 0.5*(16/9)*monitor_h 0.5*monitor_h, keep_aspect_ratio on, opacity 1.0 0.34"
 
       # floating have special active/inactive border
       "match:float true, border_color rgb(111212)"
