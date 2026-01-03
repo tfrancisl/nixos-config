78fe0bc496b8a0ecf375d2bcb2865f780c49c318
tfrancisl <tfrancislester@gmail.com>
2026-01-03 17:33:07 -0500

    Add sysctl optimations from SteamOS

--- machines/valhalla/system.nix
+++ machines/valhalla/system.nix
@@ -16,6 +16,15 @@
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
+  boot.kernel.sysctl = {
+    # Taken from https://github.com/fufexan/nix-gaming/blob/master/modules/platformOptimizations.nix
+    # SteamOS platform optimization
+    "kernel.sched_cfs_bandwidth_slice_us" = 3000;
+    "net.ipv4.tcp_fin_timeout" = 5;
+    "kernel.split_lock_mitigate" = 0;
+    "vm.max_map_count" = 2147483642;
+  };
+
   boot.loader.grub = {
     enable = true;
     device = "/dev/sda";
