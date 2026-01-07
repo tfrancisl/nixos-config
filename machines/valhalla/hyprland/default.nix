fa0958ce8ec498e3bbe2c516cc27948a71c0dd5c
Tim Lester <tfrancislester@gmail.com>
2026-01-06 22:14:18 -0500

    Simplify nix settings/trusted subs

--- machines/valhalla/hyprland/default.nix
+++ machines/valhalla/hyprland/default.nix
@@ -3,29 +3,15 @@
     enable = true;
   };
 
-  nix.settings = {
-    # get hyprland from cachix
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-
-    trusted-substituters = ["https://hyprland.cachix.org"];
-
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   imports = [
+    # Hyprland maintained nixos mod
+    # https://github.com/hyprwm/Hyprland/blob/main/flake.nix
+    # https://github.com/hyprwm/Hyprland/blob/main/nix/module.nix
+    inputs.hyprland.nixosModules.default
     ./uwsm.nix
     ./settings.nix
     ./theme.nix
     ./binds.nix
     ./rules.nix
-    # Hyprland maintained nixos mod
-    # https://github.com/hyprwm/Hyprland/blob/main/flake.nix
-    # https://github.com/hyprwm/Hyprland/blob/main/nix/module.nix
-    inputs.hyprland.nixosModules.default
   ];
 }
