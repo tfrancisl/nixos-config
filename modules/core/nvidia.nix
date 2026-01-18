f882da874740bbc601438f08d529a89760a3a07c
Tim Lester <tfrancislester@gmail.com>
2026-01-17 23:42:12 -0500

    nvidia: add boot related options

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -1,13 +1,37 @@
-{...}: {
+{config, ...}: {
   config = {
-    hardware.graphics.enable = true;
     services.xserver.videoDrivers = ["nvidia"];
-    hardware.nvidia = {
-      modesetting.enable = true;
-      open = false;
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
-      nvidiaSettings = true;
+
+    hardware = {
+      nvidia = {
+        open = false;
+        gsp.enable = config.hardware.nvidia.open;
+        nvidiaSettings = false;
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+      };
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+    };
+
+    boot = {
+      # early load / early kms
+      initrd.kernelModules = [
+        "nvidia"
+        "nvidia_modeset"
+        "nvidia_uvm"
+        "nvidia_drm"
+      ];
+      blacklistedKernelModules = ["nouveau"];
+
+      kernelParams = [
+        "nvidia.NVreg_UsePageAttributeTable=1"
+        "nvidia.NVreg_RegistryDwords=RmEnableAggressiveVblank=1" # low-latency stuff
+        "nvidia-modeset.disable_vrr_memclk_switch=1"
+      ];
     };
   };
 }
