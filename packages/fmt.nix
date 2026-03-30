bd144e8b8ed039466b8fba174084e3385418bc91
Tim Lester <tfrancislester@gmail.com>
2026-03-30 11:27:59 -0400

    chore: Update nfmt

--- packages/fmt.nix
+++ packages/fmt.nix
@@ -6,9 +6,11 @@ pkgs.writeShellApplication {
     deadnix
     statix
     fd
+    nixf-diagnose
   ];
   text = ''
     fd "$@" -t f -e nix -x statix fix -- '{}'
     fd "$@" -t f -e nix -X deadnix -e -- '{}' \; -X alejandra -q '{}'
+    fd "$@" -t f -e nix -x nixf-diagnose --auto-fix -- '{}'
   '';
 }
