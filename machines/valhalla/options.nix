4291936a4322296bd51cac6e1442786e887b9450
Tim Lester <tim-lester@idexx.com>
2026-04-02 13:31:48 -0400

    feat: set up gh, git, zed editor

--- machines/valhalla/options.nix
+++ machines/valhalla/options.nix
@@ -7,11 +7,16 @@ _: {
       enable = true;
       autologinCommand = "/run/current-system/sw/bin/start-hyprland";
     };
-    git.enable = true;
+    git = {
+      user = {
+        name = "Tim Lester";
+        email = "tfrancislester@gmail.com";
+        username = "tfrancisl";
+      };
+    };
     pipewire.enable = true;
     hyprland.enable = true;
     firefox.enable = true;
-    zed-editor.enable = true;
     claude-code.enable = true;
     gaming.enable = true;
     network = {
