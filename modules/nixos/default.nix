9f9b1bee96b15867f3d30912b7800dad8500aa2f
Tim Lester <tfrancislester@gmail.com>
2026-04-02 17:07:46 -0400

    refactor: flatten modules/nixos

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,17 @@
 {inputs, ...}: {
   imports = [
-    ./core
-    ./programs
+    ./gaming
+    ./hyprland
+    ./direnv.nix
+    ./firefox.nix
+    ./greeter.nix
+    ./network.nix
+    ./nh.nix
+    ./nvidia.nix
+    ./pipewire.nix
+    ./sudo.nix
+    ./system.nix
+    ./user.nix
     inputs.hjem.nixosModules.default
   ];
 }
