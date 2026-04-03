8d2905d38987e101eaa62f4d19ad55ea71c86eb6
Tim Lester <tfrancislester@gmail.com>
2026-04-03 19:14:40 -0400

    refactor: {hjem/user.nix} -> home.nix

--- modules/common/home.nix
+++ modules/common/home.nix
@@ -15,8 +15,15 @@ in {
     hjem = {
       linker = pkgs.smfh;
       clobberByDefault = true;
-
       users.${username}.enable = true;
     };
+    environment.variables = {
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_STATE_HOME = "$HOME/.local/state";
+    };
+    programs.fish.enable = true;
+    time.timeZone = "America/New_York"; # EST/EDT
   };
 }
---
+++ modules/darwin/home.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.acme.core) username;
+in {
+  config = {
+    hjem.users.${username} = {
+      clobberFiles = false; # a bit risky on an existing macos install
+      directory = "/Users/${username}";
+    };
+    users.users.${username} = {
+      description = "${username}'s user account";
+      shell = pkgs.fish;
+    };
+  };
+}
