fa7f352f75d92d1bb52afc22fbd04688a5aabf80
Tim Lester <tflester@tflester.com>
2026-04-17 13:42:55 -0400

    feat(direnv): enable direnv for nixos and nix-darwin

--- modules/common/direnv.nix
+++ modules/common/direnv.nix
@@ -9,7 +9,6 @@ in {
     programs.direnv = {
       enable = true;
       enableBashIntegration = false;
-      enableXonshIntegration = false;
       enableZshIntegration = false;
       nix-direnv.enable = true;
     };
