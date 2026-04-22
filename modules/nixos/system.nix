cb50ac031f3fb79e6bdc68f56f1e011dc1bfc93d
Tim Lester <tfrancislester@gmail.com>
2026-04-21 20:41:12 -0400

    feat(checks): add deadnix and statix to checks

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -5,29 +5,29 @@
 }: {
   config = {
     documentation.nixos.enable = false;
-    boot.initrd.availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "usb_storage"
-      "usbhid"
-      "sd_mod"
-    ];
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-    boot.kernelModules = ["kvm-amd"];
-
-    boot.kernel.sysctl = {
-      # Taken from https://github.com/fufexan/nix-gaming/blob/master/modules/platformOptimizations.nix
-      # SteamOS platform optimization
-      "kernel.sched_cfs_bandwidth_slice_us" = 3000;
-      "net.ipv4.tcp_fin_timeout" = 5;
-      "kernel.split_lock_mitigate" = 0;
-      "vm.max_map_count" = 2147483642;
-    };
-
-    boot.loader.grub = {
-      enable = true;
-      device = "nodev";
-      useOSProber = false;
+    boot = {
+      initrd.availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+      ];
+      kernelPackages = pkgs.linuxPackages_latest;
+      kernelModules = ["kvm-amd"];
+      kernel.sysctl = {
+        # Taken from https://github.com/fufexan/nix-gaming/blob/master/modules/platformOptimizations.nix
+        # SteamOS platform optimization
+        "kernel.sched_cfs_bandwidth_slice_us" = 3000;
+        "net.ipv4.tcp_fin_timeout" = 5;
+        "kernel.split_lock_mitigate" = 0;
+        "vm.max_map_count" = 2147483642;
+      };
+      loader.grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = false;
+      };
     };
 
     hardware.enableRedistributableFirmware = lib.mkDefault true;
