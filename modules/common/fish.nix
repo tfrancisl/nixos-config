40cf8f4a5fdb1bce8c3b892a64543a214ebeb646
Tim Lester <tfrancislester@gmail.com>
2026-05-25 01:29:00 -0400

    chore: fix/trim aliases

--- modules/common/fish.nix
+++ modules/common/fish.nix
@@ -13,21 +13,17 @@ in
     hjem.users.${username} = {
       files = {
         ".config/fish/conf.d/aliases.fish".text = ''
-          # eza
           alias ls '${getExe pkgs.eza} --group-directories-first'
-          alias ll '${getExe pkgs.eza} -l --group-directories-first'
+          alias l '${getExe pkgs.eza} -l --group-directories-first'
           alias la '${getExe pkgs.eza} -la --group-directories-first'
           alias tree '${getExe pkgs.eza} --tree --group-directories-first'
 
-          # bat
           alias cat '${getExe pkgs.bat} --plain'
         '';
         ".config/fish/conf.d/abbreviations.fish".text = ''
           # nix shortcuts — expand on space so you see the full command
           abbr --add ns 'nix shell nixpkgs#'
           abbr --add nr 'nix run nixpkgs#'
-          abbr --add ndev 'nix develop'
-          abbr --add nse 'nix search nixpkgs'
         '';
       };
     };
