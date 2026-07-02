9669ac717a7774c7121b80184ae522410851d717
Tim Lester <tfrancislester@gmail.com>
2026-07-02 13:56:30 -0400

    fix(nix): put nix config nixpkgs source on nixPath

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,19 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, nixpkgs-source, ... }:
 {
   config = {
-    nix.settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      warn-dirty = false;
-      allow-import-from-derivation = false;
-      accept-flake-config = true;
-      use-xdg-base-directories = true;
-      allowed-users = [ "@wheel" ];
-      trusted-users = [ "@wheel" ];
+    nix = {
+      package = pkgs.lixPackageSets.git.lix;
+      nixPath = [ "nixpkgs=${nixpkgs-source}" ];
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        warn-dirty = false;
+        allow-import-from-derivation = false;
+        accept-flake-config = true;
+        use-xdg-base-directories = true;
+        allowed-users = [ "@wheel" ];
+        trusted-users = [ "@wheel" ];
+      };
     };
-    nix.package = pkgs.lixPackageSets.git.lix;
 
     nixpkgs.config.allowUnfree = true;
   };
